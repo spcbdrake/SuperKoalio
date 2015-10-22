a902ae5eaec91d2b012c6d372468f98cc07ad736
oakes <zsoakes@gmail.com>
2015-10-21 21:44:42 -0400

    Add assets and modify initial code

--- desktop/src/com/theironyard/superkoalio/desktop/DesktopLauncher.java
+++ desktop/src/com/theironyard/superkoalio/desktop/DesktopLauncher.java
@@ -5,8 +5,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.theironyard.superkoalio.SuperKoalio;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SuperKoalio(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new SuperKoalio(), config);
+    }
 }
